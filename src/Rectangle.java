828cef0feefe00de789d6d3f1aa26a296c337aeb
Supriya Gole <supriyagole12@gmail.com>
2016-03-16 20:55:41 +0530

    <Nimesh, Supriya>added some behaviours of rectangle like area and perimeter

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,19 +1,28 @@
 /*
+    Job of Rectangle :-
     Rectangle should calculate area and perimeter
  */
 public class Rectangle {
     private double height;
     private double width;
 
-    public Rectangle(double height, double width) {
+    private Rectangle(double height, double width) {
         this.height = height;
         this.width = width;
     }
 
-    public double calculateArea(){
-        return height*width;
+    public static Rectangle createRectangle(double height, double width) throws InvalidInputException {
+        if (height <= 0 && width <= 0) {
+            throw new InvalidInputException();
+        } else
+            return new Rectangle(height, width);
     }
-    public double calculatePerimeter(){
-        return 2*(height+width);
+
+    public double calculateArea() {
+        return height * width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (height + width);
     }
 }
--- test/RectangleTest.java
+++ test/RectangleTest.java
@@ -4,15 +4,20 @@ import static org.junit.Assert.assertEquals;
 
 public class RectangleTest {
     @Test
-    public void calculateAreaShouldCalculateTheAreaAccordingToHeightAndWidth() {
-        Rectangle rectangle = new Rectangle(2, 3);
+    public void areaOfRectangle_should_be_natural_number_when_we_provide_height_and_width_as_a_natural_number() throws InvalidInputException {
+        Rectangle rectangle = Rectangle.createRectangle(2, 3);
         double area = rectangle.calculateArea();
         assertEquals(area,6.0,0.001);
     }
 
+    @Test(expected = InvalidInputException.class)
+    public void invalidInputException_should_generate_when_we_provide_width_and_height_as_zero_or_less_than_zero () throws InvalidInputException {
+        Rectangle.createRectangle(0,-1);
+    }
+
     @Test
-    public void calculatePerimeterShouldCalculateThePerimeterAccordingToHeightAndWidth() {
-        Rectangle rectangle = new Rectangle(2, 3);
+    public void perimeterOfRectangle_should_be_natural_number_when_we_provide_height_and_width_as_a_natural_number() throws InvalidInputException {
+        Rectangle rectangle = Rectangle.createRectangle(2, 3);
         double perimeter = rectangle.calculatePerimeter();
         assertEquals(perimeter,10.0,0.001);
     }
